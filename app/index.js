0de3c0700d161e27d03ea598e1782870277c6964
RuslanDidun <ruslandidun5@gmail.com>
2023-04-02 19:46:03 -0600

    starter layout

--- app/index.js
+++ app/index.js
@@ -1,13 +1,28 @@
-import { View, Text } from 'react-native';
+import { useSate } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 
 const Home = () => {
+
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>
-        Home
-      </Text>
-    </View>
+    <SafeAreaView styles={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <Stack.Screen option={{
+        headerStyle:{backgroundColor: COLORS.lightWhite},
+        headerShadowVisible: false,
+        headerLeft: () => (
+          <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%'/>
+        ),
+        headerRight: () => (
+          <ScreenHeaderBtn iconUrl={icons.profile} dimension='100%' />
+        ),
+        headderTitle: ""
+      }}/>
+    </SafeAreaView>
   )
 }
 
