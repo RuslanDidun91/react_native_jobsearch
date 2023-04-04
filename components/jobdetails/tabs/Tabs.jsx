8aec643d23e1fb74c243bbb53f6b471992553043
RuslanDidun <ruslandidun5@gmail.com>
2023-04-03 19:07:39 -0600

    job-details page

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { TouchableOpacity, FlatList, Text, View } from "react-native";
+import styles from "./tabs.style";
+import { SIZES } from "../../../constants";
 
-import styles from './tabs.style'
+const TabButton = ({ name, activeTab, onHandleSearchType }) => {
+  <TouchableOpacity
+    style={styles.btn(name, activeTab)}
+    onPress={onHandleSearchType}
+  >
+    <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+  </TouchableOpacity>
+};
 
-const Tabs = () => {
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
